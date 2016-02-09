cc9401076b7db38ed77528d093845a39dd5266b0
Matt Pindell <pindell.m@gmail.com>
2016-02-08 17:35:46 -0700

    created Binary Search Tree class and corresponding test

---
+++ test/binary_search_tree_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/binary_search_tree'
+require 'pry'
+
+class BinarySearchTreeTest < Minitest::Test
+
+  def setup
+    @tree = BinarySearchTree.new
+  end
+
+  def test_can_create_instance_of_binary_search_tree
+    assert_kind_of BinarySearchTree, @tree
+  end
+
+end
