705f9cdbd1b6fde3d9f049188be92d7d133ee9a0
Matt Pindell <pindell.m@gmail.com>
2016-02-08 17:30:38 -0700

    refactored node_test to utilize setup

--- lib/node.rb
+++ lib/node.rb
@@ -2,11 +2,12 @@ require 'pry'
 
 class Node
   attr_reader :score, :title
-  attr_accessor :left_link, :right_link
+  attr_accessor :depth, :left_link, :right_link
 
   def initialize(score, title)
     @score      = score
     @title      = title
+    @depth      = depth
     @left_link  = nil
     @right_link = nil
   end
--- test/node_test.rb
+++ test/node_test.rb
@@ -5,28 +5,29 @@ require 'pry'
 
 class NodeTest < Minitest::Test
 
-  def test_can_create_instance_of_node
-    root = Node.new(81, "Jaws")
+  def setup
+    @root = Node.new(81, "Jaws")
+  end
 
-    assert_kind_of Node, root
+  def test_can_create_instance_of_node
+    assert_kind_of Node, @root
   end
 
   def test_node_initializes_with_score_and_title
-    root           = Node.new(81, "Jaws")
     expected_score = 81
     expected_title = "Jaws"
 
-    assert_equal expected_score, root.score
-    assert_equal expected_title, root.title
+    assert_equal expected_score, @root.score
+    assert_equal expected_title, @root.title
   end
 
   def test_node_initializes_with_left_and_right_links_as_nil
-    root           = Node.new(81, "Jaws")
-    expected_left  = nil
-    expected_right = nil
+    assert_nil @root.left_link
+    assert_nil @root.right_link
+  end
 
-    assert_nil expected_left
-    assert_nil expected_right
+  def test_node_initializes_with_depth_as_nil
+    assert_nil @root.depth
   end
 
 
