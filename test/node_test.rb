2476bf35c6878741e80c46430a6e61b2e04aa01d
Matt Pindell <pindell.m@gmail.com>
2016-02-08 17:23:05 -0700

    test: node initialize

--- test/node_test.rb
+++ test/node_test.rb
@@ -11,5 +11,14 @@ class NodeTest < Minitest::Test
     assert_kind_of Node, root
   end
 
+  def test_node_initializes_with_score_and_title
+    root           = Node.new(81, "Jaws")
+    expected_score = 81
+    expected_title = "Jaws"
+
+    assert_equal expected_score, root.score
+    assert_equal expected_title, root.title
+  end
+
 
 end
